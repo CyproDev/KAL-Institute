77a7c1e9216566ac770641b34eb84eafb2814615
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2023-07-27 00:49:42 +0500

    Coming soon

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.png";
+
+const App = () => {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" />
+      <span>Coming soon</span>
+    </div>
+  );
+};
+
+export default App;
